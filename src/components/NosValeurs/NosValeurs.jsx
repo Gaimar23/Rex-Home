1d125de1c974f7f59389819973e0f753d71fe0a8
Gaimar23 <highdevelopment01@gmail.com>
2024-09-13 09:31:26 +0200

    second commit

---
+++ src/components/NosValeurs/NosValeurs.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./NosValeurs.scss";
+
+const NosValeurs = () => {
+  return (
+    <div className="sous-conteneur-valeurs">
+      <span className="titre">Les meilleures Offres</span>
+      <h2 className="sous-titre">Résidences les plus populaire</h2>
+    </div>
+  );
+};
+
+export default NosValeurs;
--- src/components/Produits/Produits.jsx
+++ src/components/Produits/Produits.jsx
@@ -5,27 +5,155 @@ import { images } from "../../autres/data";
 const Produits = () => {
   return (
     <div className="sous-conteneur-produits">
-      <span>Les meilleures Offres</span>
-      <h2>Résidences les plus populaire</h2>
+      <span className="titre">Les meilleures Offres</span>
+      <h2 className="sous-titre">Résidences les plus populaire</h2>
       <div className="conteneur-slide">
         <div className="suivi-slide">
           <div className="carte-article">
             <img src={images.home4} alt="" />
+            <div className="details">
+              <span className="prix">
+                <span>$</span>1000
+              </span>
+              <h4 className="nom"> Rize de Sana</h4>
+              <p className="desc">
+                Un magnifique et spacieux appartement pour vous et votre famille
+              </p>
+            </div>
           </div>
           <div className="carte-article">
             <img src={images.home5} alt="" />
+            <div className="details">
+              <span className="prix">
+                <span>$</span>1000
+              </span>
+              <h4 className="nom"> Rize de Sana</h4>
+              <p className="desc">
+                Un magnifique et spacieux appartement pour vous et votre famille
+              </p>
+            </div>
           </div>
           <div className="carte-article">
             <img src={images.home6} alt="" />
+            <div className="details">
+              <span className="prix">
+                <span>$</span>1000
+              </span>
+              <h4 className="nom"> Rize de Sana</h4>
+              <p className="desc">
+                Un magnifique et spacieux appartement pour vous et votre famille
+              </p>
+            </div>
           </div>
           <div className="carte-article">
             <img src={images.home7} alt="" />
+            <div className="details">
+              <span className="prix">
+                <span>$</span>1000
+              </span>
+              <h4 className="nom"> Rize de Sana</h4>
+              <p className="desc">
+                Un magnifique et spacieux appartement pour vous et votre famille
+              </p>
+            </div>
           </div>
           <div className="carte-article">
             <img src={images.home8} alt="" />
+            <div className="details">
+              <span className="prix">
+                <span>$</span>1000
+              </span>
+              <h4 className="nom"> Rize de Sana</h4>
+              <p className="desc">
+                Un magnifique et spacieux appartement pour vous et votre famille
+              </p>
+            </div>
           </div>
           <div className="carte-article">
             <img src={images.home3} alt="" />
+            <div className="details">
+              <span className="prix">
+                <span>$</span>1000
+              </span>
+              <h4 className="nom"> Rize de Sana</h4>
+              <p className="desc">
+                Un magnifique et spacieux appartement pour vous et votre famille
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="suivi-slide">
+          <div className="carte-article">
+            <img src={images.home4} alt="" />
+            <div className="details">
+              <span className="prix">
+                <span>$</span>1000
+              </span>
+              <h4 className="nom"> Rize de Sana</h4>
+              <p className="desc">
+                Un magnifique et spacieux appartement pour vous et votre famille
+              </p>
+            </div>
+          </div>
+          <div className="carte-article">
+            <img src={images.home5} alt="" />
+            <div className="details">
+              <span className="prix">
+                <span>$</span>1000
+              </span>
+              <h4 className="nom"> Rize de Sana</h4>
+              <p className="desc">
+                Un magnifique et spacieux appartement pour vous et votre famille
+              </p>
+            </div>
+          </div>
+          <div className="carte-article">
+            <img src={images.home6} alt="" />
+            <div className="details">
+              <span className="prix">
+                <span>$</span>1000
+              </span>
+              <h4 className="nom"> Rize de Sana</h4>
+              <p className="desc">
+                Un magnifique et spacieux appartement pour vous et votre famille
+              </p>
+            </div>
+          </div>
+          <div className="carte-article">
+            <img src={images.home7} alt="" />
+            <div className="details">
+              <span className="prix">
+                <span>$</span>1000
+              </span>
+              <h4 className="nom"> Rize de Sana</h4>
+              <p className="desc">
+                Un magnifique et spacieux appartement pour vous et votre famille
+              </p>
+            </div>
+          </div>
+          <div className="carte-article">
+            <img src={images.home8} alt="" />
+            <div className="details">
+              <span className="prix">
+                <span>$</span>1000
+              </span>
+              <h4 className="nom"> Rize de Sana</h4>
+              <p className="desc">
+                Un magnifique et spacieux appartement pour vous et votre famille
+              </p>
+            </div>
+          </div>
+          <div className="carte-article">
+            <img src={images.home3} alt="" />
+            <div className="details">
+              <span className="prix">
+                <span>$</span>1000
+              </span>
+              <h4 className="nom"> Rize de Sana</h4>
+              <p className="desc">
+                Un magnifique et spacieux appartement pour vous et votre famille
+              </p>
+            </div>
           </div>
         </div>
       </div>
